a7148282e4ae41ee86a7cbd6767d4ca479887b50
Allen <tosakaen10254@gmail.com>
2025-11-26 17:08:49 +0800

    feat: implement canvas purchase system with revenue distribution
    - Add user purchase limits (100 total, 1 per user)
    - Platform pre-mints all NFTs
    - Revenue flows to Revenue contract for distribution
    - Remove .DS_Store and update .gitignore

--- contract/core/FemCanvas.sol
+++ contract/core/FemCanvas.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "../interface/IFemCanvas.sol";
+import "../interface/IFemCanvasRevenue.sol";
 
 contract FemCanvas is IFemCanvas, ERC1155, Ownable, ReentrancyGuard {
     uint256 public canvasCounter;
@@ -15,6 +16,14 @@ contract FemCanvas is IFemCanvas, ERC1155, Ownable, ReentrancyGuard {
     // dayTimestamp => canvasId      
     mapping(uint256 => uint256) public dayToCanvasId;
     mapping(address => bool) public authorizedMinters;
+    
+    // 新增：用户购买相关
+    mapping(uint256 => uint256) public canvasPrice;           // 画布价格
+    mapping(uint256 => uint256) public publicMinted;          // 已公开销售数量
+    mapping(uint256 => mapping(address => bool)) public hasMinted; // 用户是否已购买
+    uint256 public constant PUBLIC_SUPPLY_LIMIT = 100;        // 公开销售限制
+    
+    IFemCanvasRevenue public revenueContract;                 // Revenue合约地址
 
     modifier onlyAuthorized() {
         require(authorizedMinters[msg.sender] || msg.sender == owner(), "Not authorized");
@@ -27,6 +36,16 @@ contract FemCanvas is IFemCanvas, ERC1155, Ownable, ReentrancyGuard {
 
     function setAuthorizedMinter(address minter, bool authorized) external onlyOwner {
         authorizedMinters[minter] = authorized;
+    }
+    
+    function setRevenueContract(address _revenueContract) external onlyOwner {
+        require(_revenueContract != address(0), "Invalid revenue contract");
+        revenueContract = IFemCanvasRevenue(_revenueContract);
+    }
+    
+    function setCanvasPrice(uint256 canvasId, uint256 price) external onlyOwner {
+        require(canvases[canvasId].canvasId != 0, "Canvas does not exist");
+        canvasPrice[canvasId] = price;
     }
 
     function mintCanvas(uint256 canvasId,uint256 dayTimestamp,string calldata _metadataURI,uint256 supply) external payable override{
@@ -49,9 +68,10 @@ contract FemCanvas is IFemCanvas, ERC1155, Ownable, ReentrancyGuard {
         canvasCounter++;
         canvasCounterMapping[canvasId] = canvasCounter;
 
-        _mint(msg.sender, canvasId, supply, "");
+        // 平台预铸造所有NFT
+        _mint(owner(), canvasId, supply, "");
         canvases[canvasId].totalRaised += msg.value;
-        payable(owner()).transfer(msg.value);
+        // 不立即转账，留在合约中
         
         emit CanvasCreated(canvasId, dayTimestamp, _metadataURI, msg.sender,canvasCounter);
     }
@@ -105,5 +125,38 @@ contract FemCanvas is IFemCanvas, ERC1155, Ownable, ReentrancyGuard {
 
     function getCurrentCanvasCounter() external view returns (uint256) {
         return canvasCounter;
+    }
+    
+    function buyCanvas(uint256 canvasId) external payable nonReentrant {
+        require(canvases[canvasId].canvasId != 0, "Canvas does not exist");
+        require(canvases[canvasId].finalized, "Canvas not finalized");
+        require(publicMinted[canvasId] < PUBLIC_SUPPLY_LIMIT, "Sold out");
+        require(!hasMinted[canvasId][msg.sender], "Already minted");
+        require(msg.value >= canvasPrice[canvasId], "Insufficient payment");
+        require(address(revenueContract) != address(0), "Revenue contract not set");
+        
+        // 从平台转给用户
+        _safeTransferFrom(owner(), msg.sender, canvasId, 1, "");
+        
+        publicMinted[canvasId]++;
+        hasMinted[canvasId][msg.sender] = true;
+        canvases[canvasId].totalRaised += msg.value;
+        
+        // 将收益发送到Revenue合约
+        revenueContract.receiveRevenue{value: msg.value}(canvasId);
+        
+        emit CanvasCreated(canvasId, canvases[canvasId].dayTimestamp, canvases[canvasId].metadataURI, msg.sender, canvasCounter);
+    }
+    
+    function getCanvasPurchaseInfo(uint256 canvasId) external view returns (
+        uint256 price,
+        uint256 minted,
+        uint256 remaining,
+        bool userHasMinted
+    ) {
+        price = canvasPrice[canvasId];
+        minted = publicMinted[canvasId];
+        remaining = PUBLIC_SUPPLY_LIMIT - minted;
+        userHasMinted = hasMinted[canvasId][msg.sender];
     }
 }
